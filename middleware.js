66f033a274812415b9a5554562e4f85525dc0363
Bruno Sores Machado <122398527+sbrunomachado@users.noreply.github.com>
2023-05-22 17:16:25 -0300

    vuish

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 const Allow = (handler) => (req, res) => {
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.headers.set("Access-Control-Max-Age", "86400");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Max-Age", "86400");
 
     if(req.method === 'OPTIONS'){
         res.status(200).end();
