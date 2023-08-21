ed2c89dc5ef05d29a7809d12975fad8fa4d00233
Bruno Sores Machado <122398527+sbrunomachado@users.noreply.github.com>
2023-08-21 10:05:38 -0300

    eu tenho q ter mta paciência em véi

--- middleware.js
+++ middleware.js
@@ -23,7 +23,7 @@ const Allow = (handler) => async (req, res) => {
   console.log(token)
   const id = req.headers['id'];
   console.log(id)
-  const origin = req.get('Origin');
+  const origin = req.headers['Origin'];
   console.log(origin)
 
     if (id === 'vt') {
